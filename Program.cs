1cb2c7ad9b04aaf022a350fc2dda7ab245a6c2b4
OlegSs <youremail@yourdomain.comolegs934@gmail.com>
2022-10-05 16:05:31 +0300

    Задача решена

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+if (b < a && a > c)
+{
+    Console.WriteLine("Первое число самое боьшое");
+}
+else if (a < b && b > c)
+{
+    Console.WriteLine("Второе число самое боьшое");
+}
+else if (b < c && c > a)
+{
+    Console.WriteLine("Третье число самое большое");
+}
